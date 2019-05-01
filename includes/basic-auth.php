970dc7ef02bca517cad6a4820b69005aa20630a0
Jason Coleman <jason@strangerstudios.com>
2019-04-30 20:47:41 -0400

    use rest_get_url_prefix

--- includes/basic-auth.php
+++ includes/basic-auth.php
@@ -14,11 +14,11 @@ function wpsso_json_basic_auth_handler( $user ) {
 
 	// Don't run unless using our route.	
 	if ( ! empty( $_REQUEST['rest_route'] ) ) {
-		$rest_route = '/wp-json' . $_REQUEST['rest_route'];
+		$rest_route = '/' . rest_get_url_prefix() . $_REQUEST['rest_route'];
 	} else {
 		$rest_route = $_SERVER['REQUEST_URI'];
 	}
-	if ( $rest_route != '/wp-json/wp-sso/v1/check' ) {
+	if ( $rest_route != '/' . rest_get_url_prefix() . '/wp-sso/v1/check' ) {
 		return $user;
 	}
 
