d1d921894e2b05c366938b352bec535300d194cf
Jason Coleman <jason@strangerstudios.com>
2022-07-22 22:13:13 +1000

    version had to be a string here

--- wp-sso.php
+++ wp-sso.php
@@ -3,13 +3,13 @@
  * Plugin Name: WP Single Sign On (SSO)
  * Plugin URI: https://www.strangerstudios.com/wp-sso/
  * Description: Connect multiple non-multisite WordPress sites together to share logins.
- * Version: .1.1
+ * Version: 0.1.1
  * Author: strangerstudios
  * Author URI: https://www.strangerstudios.com
  * Text Domain: wp-sso
  */
 
-define( 'WP_SSO_VERSION', .1.1 );
+define( 'WP_SSO_VERSION', '0.1.1' );
 define( 'WP_SSO_DIR', dirname( __FILE__ ) );
 define( 'WP_SSO_BASENAME', plugin_basename( __FILE__ ) );
 
